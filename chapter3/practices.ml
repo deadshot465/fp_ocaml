cdae9ac4a9449c567e11e136d181164deece51f8
Chehui Chou <deadshot465@gmail.com>
2021-04-02 13:28:35 +0900

    Add chapter3 (not finished).

---
+++ chapter3/practices.ml
@@ -0,0 +1,36 @@
+let list_1 = [1; 2; 3; 4; 5]
+let list_2 = 1 :: 2 :: 3 :: 4 :: 5 :: []
+let list_3 = [1] @ [2; 3; 4] @ [5]
+
+let rec product acc lst =
+  match lst with
+  [] -> acc
+  | x::xs -> product (acc * x) xs
+
+let rec concat acc lst =
+  match lst with
+  [] -> acc
+  | x::xs -> concat (acc ^ x) xs
+
+let is_first_bigred = function
+  [] -> false
+  | x::_ -> x = "bigred"
+
+let has_two_or_four_elems lst =
+  match lst with
+  a::b::[] -> true
+  | a::b::c::d::[] -> true
+  | _ -> false
+
+let is_first_two_equal lst =
+  match lst with
+  x::y::_ -> x = y
+  | _ -> false
+
+let get_fifth lst = if List.length lst < 5 then 0 else List.nth lst 4
+
+let descend_list lst = List.rev (List.sort Stdlib.compare lst)
+
+let get_last lst = List.nth lst ((List.length lst) - 1)
+
+let any_zeroes lst = List.mem 0 lst
